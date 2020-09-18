b3c65a14534537ab55bad494bfdd218dbb45aa1d
Tom Overland <tomoverland03@gmail.com>
2020-09-18 11:07:23 -0500

    fixed error with inquirer confirm

--- Develop/app.js
+++ Develop/app.js
@@ -1,4 +1,4 @@
-const Employee = require('./lib/Employee')
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -15,83 +15,83 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 const questions = [
-    { name: 'name', message: "What is the employee's name?" },
-    { name: "id", message: "What's the employee's ID?" },
-    { name: "email", message: "What is the employee's email?" },
-    { 
-        type: "list",
-        name:  "role",
-        message: "What's the employee's role?",
-        choices: ["Manager", "Engineer", "Intern"],
-    },
-
+  { name: "name", message: "What is the employee's name?" },
+  { name: "id", message: "What's the employee's ID?" },
+  { name: "email", message: "What is the employee's email?" },
+  {
+    type: "list",
+    name: "role",
+    message: "What's the employee's role?",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
 ];
 
 const managerQuestions = [
-    { name: "officeNumber", message: "What's the manager's office number?" },
+  { name: "officeNumber", message: "What's the manager's office number?" },
 ];
 
 const engineerQuestions = [
-    { name: 'github', message: "What's is the engineer's Github account?"},
+  { name: "github", message: "What's is the engineer's Github account?" },
 ];
 
 const internQuestions = [
-    { name: 'school', message: "What school does the intern attend?" }
+  { name: "school", message: "What school does the intern attend?" },
 ];
 
 const confirm = [
-    { type: "confirm", name: "continue", message: "Do you want to input more employee information?" },
+  {
+    type: "confirm",
+    name: "addEmployee",
+    message: "Do you want to input more employee information?",
+  },
 ];
 
 const init = async () => {
-    const employees = [];
-    let newEmployee = true;
+  const employees = [];
+  let newEmployee = true;
 
-    while (newEmployee) {
-        const { name, id, email, role } = await inquirer.prompt(questions);
+  while (newEmployee) {
+    const { name, id, email, role } = await inquirer.prompt(questions);
 
-        if (role === 'Manager') {
-            const {officeNumber} = await inquirer.prompt(managerQuestions);
-            //create new manager object and push to employees array
-            employees.push(new Manager(name, id, email, officeNumber));
-        } else if (role === 'Engineer') {
-            const { github } = await inquirer.prompt(engineerQuestions);
-            //create new engineer object and push to employees array
-            employees.push(new Engineer(name, id, email, github));
-        } else {
-            const { school } = await inquirer.prompt(internQuestions);
-            //create new intern object and push to employees array
-            employees.push(new Intern(name, id, email, school));
-        }
-        // addEmployee variable will return a boolean value based on user response to input more info, will end inputs on false
-        const { addEmployee } = await inquirer.prompt(confirm);
-
-        newEmployee = addEmployee;
+    if (role === "Manager") {
+      const { officeNumber } = await inquirer.prompt(managerQuestions);
+      //create new manager object and push to employees array
+      employees.push(new Manager(name, id, email, officeNumber));
+    } else if (role === "Engineer") {
+      const { github } = await inquirer.prompt(engineerQuestions);
+      //create new engineer object and push to employees array
+      employees.push(new Engineer(name, id, email, github));
+    } else {
+      const { school } = await inquirer.prompt(internQuestions);
+      //create new intern object and push to employees array
+      employees.push(new Intern(name, id, email, school));
     }
-    // After the user has input all employees desired, call the `render` function (required
-    // above) and pass in an array containing all employee objects; the `render` function will
-    // generate and return a block of HTML including templated divs for each employee!
-    const html = render(employees);
+    // addEmployee variable will return a boolean value based on user response to input more info, will end inputs on false
+    const { addEmployee } = await inquirer.prompt(confirm);
 
-    // After you have your html, you're now ready to create an HTML file using the HTML
-    // returned from the `render` function. Now write it to a file named `team.html` in the
-    // `output` folder. You can use the variable `outputPath` above target this location.
-    // Hint: you may need to check if the `output` folder exists and create it if it
-    // does not.
-    if (!fs.existsSync(outputPath)) {
-        const error = await mkdirAsync(OUTPUT_DIR);
-        error && console.error(error);
-    };
+    newEmployee = addEmployee;
+  }
+  // After the user has input all employees desired, call the `render` function (required
+  // above) and pass in an array containing all employee objects; the `render` function will
+  // generate and return a block of HTML including templated divs for each employee!
+  const html = render(employees);
 
-    const error = await writeFileAsync(outputPath, html);
+  // After you have your html, you're now ready to create an HTML file using the HTML
+  // returned from the `render` function. Now write it to a file named `team.html` in the
+  // `output` folder. You can use the variable `outputPath` above target this location.
+  // Hint: you may need to check if the `output` folder exists and create it if it
+  // does not.
+  if (!fs.existsSync(outputPath)) {
+    const error = await mkdirAsync(OUTPUT_DIR);
     error && console.error(error);
+  }
+
+  const error = await writeFileAsync(outputPath, html);
+  error && console.error(error);
 };
-
-
 
 init();
 // HINT: each employee type (manager, engineer, or intern) has slightly different
